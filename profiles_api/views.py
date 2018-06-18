1711a03703c657a036506a3e89d63c703ef271a8
LuisAlbertoPerezDeLaCruz <luisalbertoperez@gmail.com>
2018-06-18 09:10:50 -0400

    Added a post method to our test ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,11 +5,16 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """returns a list of API featrures"""
@@ -22,3 +27,15 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self,request):
+        """Create a hello message with our name"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
