108c8496960455b70a9534ed5c11489e350be53b
LuisAlbertoPerezDeLaCruz <luisalbertoperez@gmail.com>
2018-06-23 13:55:25 -0400

    Added create,retreive, update, partial_update and destroy methods to our viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,9 +6,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
-urlpatterns=[
-   url(r'^hello-view/',views.HelloApiView.as_view()),
-   url(r'',include(router.urls)),
+urlpatterns = [
+    url(r'^hello-view/', views.HelloApiView.as_view()),
+    url(r'', include(router.urls)),
 ]
